5368c4a36d7e74878fd172c89fd1aa24ec2c97ca
Ali Askar <ali1995askar@gmail.com>
2021-07-13 05:01:00 -0400

    Configured Postgresql db.

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,13 @@ ENV PYTHONUNBUFFERED 1
 
 
 COPY ./requierements.txt /requierements.txt
+RUN echo "nameserver 8.8.8.8" > /etc/resolv.conf & apk add --update --no-cache postgresql-client
+RUN echo "nameserver 8.8.8.8" > /etc/resolv.conf & apk add --update --no-cache --virtual .tmp-build-deps  \
+    gcc libc-dev linux-headers postgresql-dev
+
 RUN echo "nameserver 8.8.8.8" > /etc/resolv.conf & pip install -r /requierements.txt
- 
+RUN apk del .tmp-build-deps
+
 RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5xew&bn490tannhbhkoa=4_r+eywh9i5cr@t2sqmq@684#_-%8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,11 +75,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': os.environ.get('DB_HOST'),
+            'NAME': os.environ.get('DB_NAME') , 
+            'USER': os.environ.get('DB_USER') ,
+            'PASSWORD': os.environ.get('DB_PASS') ,
+        }
+   
 }
+
+#  'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+
 
 
 # Password validation
